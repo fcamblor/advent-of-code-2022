46ee429517f2ab6d77ae47ba5e3dfc1954d998a5
fcamblor <fcamblor@gmail.com>
2022-12-03 14:57:26 +0100

    implemented D01_elfMaxCalories maching D01 Q1

--- src/2022-01.ts
+++ src/2022-01.ts
@@ -5,3 +5,12 @@
 export function D01_extractElfFood(str: string): Array<number[]> {
     return str.split("\n\n").map(elfValues => elfValues.split("\n").map(Number));
 }
+
+export function D01_elfTotalCalories(calories: number[]): number {
+    return calories.reduce((tot, calorie) => tot+calorie, 0)
+}
+
+export function D01_elfMaxCalories(str: string): number {
+    const elfFood = D01_extractElfFood(str);
+    return Math.max(...elfFood.map(D01_elfTotalCalories))
+}
--- test/2022-01.test.ts
+++ test/2022-01.test.ts
@@ -1,6 +1,6 @@
 
 import {D01_INPUT, D01_Q1_SAMPLE} from "./2022-01.inputs";
-import {D01_extractElfFood} from "../src/2022-01";
+import {D01_elfMaxCalories, D01_extractElfFood} from "../src/2022-01";
 
 
 test("D01_extractElfFood", () => {
@@ -12,15 +12,15 @@ test("D01_extractElfFood", () => {
         [10000]
     ])
 })
-/*
+
 test("Q1 Sample", () => {
-    expect(`an imported sample ${D01_Q1_SAMPLE} from 2022-01.inputs.ts which is then processed by src/2022-01.ts functions`).toEqual(`an expected result from 2022-01.inputs.ts`);
+    expect(D01_elfMaxCalories(D01_Q1_SAMPLE)).toEqual(24000);
 })
 
 test("Q1 Input", () => {
-    expect(`an imported sample ${D01_INPUT} from 2022-01.inputs.ts which is then processed by src/2022-01.ts functions`).toEqual(`an expected result from 2022-01.inputs.ts`);
+    expect(D01_elfMaxCalories(D01_INPUT)).toEqual(69693);
 })
-
+/*
 test("Q2 Sample ", () => {
     expect(`an imported sample ${D01_Q2_SAMPLE} from 2022-01.inputs.ts which is then processed by src/2022-01.ts functions`).toEqual(`an expected result from 2022-01.inputs.ts`);
 })

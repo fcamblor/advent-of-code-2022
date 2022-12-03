f36104b09dd5071d5dc629f9eeb013cbd105f68a
fcamblor <fcamblor@gmail.com>
2022-12-03 15:29:36 +0100

    allowing to pass a D02RoundOutcomes to D02_totalScoreOf => implemented D02 Q2

--- src/2022-02.ts
+++ src/2022-02.ts
@@ -14,6 +14,17 @@ export const D02_Q1ROUND_OUTCOMES: D02RoundOutcomes = {
     "C Y": {shapeScore: 2, outcomeScore: 0},
     "C Z": {shapeScore: 3, outcomeScore: 3},
 } as const;
+export const D02_Q2ROUND_OUTCOMES: D02RoundOutcomes = {
+    "A X": {shapeScore: 3, outcomeScore: 0},
+    "A Y": {shapeScore: 1, outcomeScore: 3},
+    "A Z": {shapeScore: 2, outcomeScore: 6},
+    "B X": {shapeScore: 1, outcomeScore: 0},
+    "B Y": {shapeScore: 2, outcomeScore: 3},
+    "B Z": {shapeScore: 3, outcomeScore: 6},
+    "C X": {shapeScore: 2, outcomeScore: 0},
+    "C Y": {shapeScore: 3, outcomeScore: 3},
+    "C Z": {shapeScore: 1, outcomeScore: 6},
+} as const;
 
 export function D02_totalScoreOf(value: string, roundOutcomes: D02RoundOutcomes = D02_Q1ROUND_OUTCOMES): number {
     const rounds = value.split("\n") as Array<keyof D02RoundOutcomes>;
--- test/2022-02.test.ts
+++ test/2022-02.test.ts
@@ -1,6 +1,6 @@
 
 import {D02_INPUT, D02_Q1_SAMPLE} from "./2022-02.inputs";
-import {D02_Q1ROUND_OUTCOMES, D02_totalScoreOf} from "../src/2022-02";
+import {D02_Q1ROUND_OUTCOMES, D02_Q2ROUND_OUTCOMES, D02_totalScoreOf} from "../src/2022-02";
 
 
 test("D02_roundOf", () => {
@@ -16,13 +16,10 @@ test("Q1 Input", () => {
     expect(D02_totalScoreOf(D02_INPUT)).toEqual(12535);
 })
 
-/*
-test("Q2 Sample ", () => {
-    expect(`an imported sample ${D02_Q2_SAMPLE} from 2022-02.inputs.ts which is then processed by src/2022-02.ts functions`).toEqual(`an expected result from 2022-02.inputs.ts`);
+test("Q2 Sample", () => {
+    expect(D02_totalScoreOf(D02_Q1_SAMPLE, D02_Q2ROUND_OUTCOMES)).toEqual(12);
 })
 
-test("Q2 Input ", () => {
-    expect(`an imported sample ${D02_INPUT} from 2022-02.inputs.ts which is then processed by src/2022-02.ts functions`).toEqual(`an expected result from 2022-02.inputs.ts`);
+test("Q2 Input", () => {
+    expect(D02_totalScoreOf(D02_INPUT, D02_Q2ROUND_OUTCOMES)).toEqual(15457);
 })
-*/
-

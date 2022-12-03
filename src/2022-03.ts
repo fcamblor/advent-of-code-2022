3eb09f0d0683236bd406639f072e5f281677cc94
fcamblor <fcamblor@gmail.com>
2022-12-03 13:44:22 +0100

    init 2022-03

---
+++ src/2022-03.ts
@@ -0,0 +1,4 @@
+
+// TODO: Fill into this file utility functions to display stuff into the spreadsheet
+// ... or simply function that are going to be tested into corresponding ../test/2022-03.test.ts file
+
---
+++ test/2022-03.test.ts
@@ -0,0 +1,20 @@
+
+import {D03_INPUT, D03_Q1_SAMPLE, D03_Q2_SAMPLE} from "./2022-03.inputs";
+
+/*
+test("Q1 Sample", () => {
+    expect(`an imported sample ${D03_Q1_SAMPLE} from 2022-03.inputs.ts which is then processed by src/2022-03.ts functions`).toEqual(`an expected result from 2022-03.inputs.ts`);
+})
+
+test("Q1 Input", () => {
+    expect(`an imported sample ${D03_INPUT} from 2022-03.inputs.ts which is then processed by src/2022-03.ts functions`).toEqual(`an expected result from 2022-03.inputs.ts`);
+})
+
+test("Q2 Sample ", () => {
+    expect(`an imported sample ${D03_Q2_SAMPLE} from 2022-03.inputs.ts which is then processed by src/2022-03.ts functions`).toEqual(`an expected result from 2022-03.inputs.ts`);
+})
+
+test("Q2 Input ", () => {
+    expect(`an imported sample ${D03_INPUT} from 2022-03.inputs.ts which is then processed by src/2022-03.ts functions`).toEqual(`an expected result from 2022-03.inputs.ts`);
+})
+*/

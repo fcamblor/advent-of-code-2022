505b0b2f6350d89582027fc18c8a1d308300ed31
fcamblor <fcamblor@gmail.com>
2022-12-04 15:39:22 +0100

    implemented D04 Q1 thanks to D04_countSectionsOverlaps

--- src/2022-04.ts
+++ src/2022-04.ts
@@ -2,14 +2,21 @@
 // TODO: Fill into this file utility functions to display stuff into the spreadsheet
 // ... or simply function that are going to be tested into corresponding ../test/2022-04.test.ts file
 
-import type {Ranged} from "./utils";
+
+import {Ranged} from "./utils";
 
 export function D04_parseRanges(str: string): Array<Ranged<number>[]> {
     return str.split("\n")
         .map(line =>
             line.split(",").map(rangeStr => {
                 const [low, up] = rangeStr.split("-");
-                return {lowerBound: `included:${Number(low)}`, upperBound: `included:${Number(up)}`};
+                return new Ranged(Number(low), true, Number(up), true);
             })
         )
+}
+
+export function D04_countSectionsOverlaps(sectionGroups: Array<Ranged<number>[]>): number {
+    return sectionGroups.reduce((total, sectionGroup) => {
+        return total + (Ranged.countFullOverlaps(sectionGroup).length?1:0);
+    }, 0)
 }
--- test/2022-04.test.ts
+++ test/2022-04.test.ts
@@ -1,26 +1,26 @@
 
 import {D04_INPUT, D04_Q1_SAMPLE, D04_Q2_SAMPLE} from "./2022-04.inputs";
-import {D04_parseRanges} from "../src/2022-04";
+import {D04_countSectionsOverlaps, D04_parseRanges} from "../src/2022-04";
 // import {D04_superFunctionForQ1, D04_superFunctionForQ2} from "../src/2022-04";
 
 test("input parsing", () => {
     expect(D04_parseRanges(`2-4,6-8`)).toEqual([
         [
-            {lowerBound: `included:2`, upperBound: `included:4`},
-            {lowerBound: `included:6`, upperBound: `included:8`},
+            {lowerBound: 2, lowerIncluded: true , upperBound: 4, upperIncluded: true },
+            {lowerBound: 6, lowerIncluded: true , upperBound: 8, upperIncluded: true },
         ]
     ])
 })
 
-/*
 test("Q1 Sample", () => {
-    expect(D04_superFunctionForQ1(D04_Q1_SAMPLE)).toEqual(42);
+    expect(D04_countSectionsOverlaps(D04_parseRanges(D04_Q1_SAMPLE))).toEqual(2);
 })
 
 test("Q1 Input", () => {
-    expect(D04_superFunctionForQ1(D04_INPUT)).toEqual(42);
+    expect(D04_countSectionsOverlaps(D04_parseRanges(D04_INPUT))).toEqual(450);
 })
 
+/*
 test("Q2 Sample", () => {
     expect(D04_superFunctionForQ2(D04_Q2_SAMPLE)).toEqual(42);
 })

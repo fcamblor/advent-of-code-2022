9aca2d82e43e2f4404fff54142be11ebf19dacf4
fcamblor <fcamblor@gmail.com>
2022-12-05 13:55:05 +0100

    implemented D05ReadStartingStacks

--- src/2022-05.ts
+++ src/2022-05.ts
@@ -11,3 +11,16 @@ export type D05Statements = {
 export function D05ReadStackIdsCount(line: string): number {
     return line.split(" ").filter(v => !!v).length;
 }
+
+export function D05ReadStartingStacks(stacksCount: number, stackLines: string[]): Array<string[]> {
+    const startingReversedStacks = Array(stacksCount+1).fill(0).map(_ => [] as string[]);
+    stackLines.map(stackLineStr => {
+        for(let i=0; i<stacksCount; i++) {
+            const crate = stackLineStr[i*"[?] ".length + "[".length];
+            if(crate !== ' ') {
+                startingReversedStacks[i+1].push(crate);
+            }
+        }
+    })
+    return startingReversedStacks.map(reversedStack => reversedStack.reverse());
+}
--- test/2022-05.test.ts
+++ test/2022-05.test.ts
@@ -1,10 +1,21 @@
 
 import {D05_INPUT, D05_Q1_SAMPLE, D05_Q2_SAMPLE} from "./2022-05.inputs";
-import {D05ReadStackIdsCount} from "../src/2022-05";
+import {D05ReadStackIdsCount, D05ReadStartingStacks} from "../src/2022-05";
 // import {D05_superFunctionForQ1, D05_superFunctionForQ2} from "../src/2022-05";
 
 test("stack sizes", () => {
     expect(D05ReadStackIdsCount(` 1   2   3 `)).toEqual(3);
+})
+
+test(`reading starting stacks`, () => {
+    expect(D05ReadStartingStacks(3, `    [D]    
+[N] [C]    
+[Z] [M] [P]`.split("\n"))).toEqual([
+        [],
+        ['Z','N'],
+        ['M','C','D'],
+        ['P']
+    ])
 })
 
 /*

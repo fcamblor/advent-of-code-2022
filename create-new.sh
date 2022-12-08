f301c0f6588a4c7f5e4622e8d5e39bd93e1c77ea
fcamblor <fcamblor@gmail.com>
2022-12-08 09:50:55 +0100

    removed Q2 sample and made generated test more prone to non modification by sharing code between q1/q2 and input tests

--- create-new.sh
+++ create-new.sh
@@ -8,7 +8,7 @@ cat > "src/$year-$day.ts" << ENDOFFILE
 // TODO: Fill into this file utility functions to display stuff into the spreadsheet
 // ... or simply function that are going to be tested into corresponding ../test/$year-$day.test.ts file
 
-export function D${day}_superFunctionForQ1(str: string) {
+export function D${day}_parseInput(input: string) {
     return 42
 }
 
@@ -16,25 +16,27 @@ ENDOFFILE
 
 cat > "test/$year-$day.test.ts" << ENDOFFILE
 
-import {D${day}_INPUT, D${day}_Q1_SAMPLE, D${day}_Q2_SAMPLE} from "./$year-$day.inputs";
+import {D${day}_INPUT, D${day}_Q1_SAMPLE} from "./$year-$day.inputs";
 // import {D${day}_superFunctionForQ1, D${day}_superFunctionForQ2} from "../src/$year-$day";
 
 /*
-test("Q1 Sample", () => {
-    expect(D${day}_superFunctionForQ1(D${day}_Q1_SAMPLE)).toEqual(42);
-})
+[
+  {name: "Q1 Sample", sample: D${day}_Q1_SAMPLE, expectation: 42},
+  {name: "Q1 Input", sample: D${day}_INPUT, expectation: 42}
+].forEach(descriptor => {
+  test(descriptor.name, () => {
+      expect(D${day}_parseInput(descriptor.sample)).toEqual(descriptor.expectation);
+  })
+});
 
-test("Q1 Input", () => {
-    expect(D${day}_superFunctionForQ1(D${day}_INPUT)).toEqual(42);
-})
-
-test("Q2 Sample", () => {
-    expect(D${day}_superFunctionForQ2(D${day}_Q2_SAMPLE)).toEqual(42);
-})
-
-test("Q2 Input", () => {
-    expect(D${day}_superFunctionForQ2(D${day}_INPUT)).toEqual(42);
-})
+[
+  {name: "Q2 Sample", sample: D${day}_Q1_SAMPLE, expectation: 42},
+  {name: "Q2 Input", sample: D${day}_INPUT, expectation: 42}
+].forEach(descriptor => {
+  test(descriptor.name, () => {
+      expect(D${day}_parseInput(descriptor.sample)).toEqual(descriptor.expectation);
+  })
+});
 
 */
 
@@ -43,11 +45,6 @@ ENDOFFILE
 cat > "test/$year-$day.inputs.ts" << ENDOFFILE
 
 export const D${day}_Q1_SAMPLE = \`
-blabla
-blabla
-\`.trim()
-
-export const D${day}_Q2_SAMPLE = \`
 blabla
 blabla
 \`.trim()

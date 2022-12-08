00b88599284adf9e8c4b79533f360897de5fe25d
fcamblor <fcamblor@gmail.com>
2022-12-08 09:49:35 +0100

    implemented D08 Q2

--- src/2022-08.ts
+++ src/2022-08.ts
@@ -48,3 +48,42 @@ export function D08_countVisibleTrees(grid: number[][]): number {
         return total + (D08_isTreePositionVisible(grid, [i,j])?1:0)
     }, 0)
 }
+
+type TreeScenicScore = { treeHeight: number, total: number, leftScore: number, rightScore: number, upScore: number, downScore: number };
+export function D08_scenicScoreOf(grid: number[][], [i,j]: [number,number]): TreeScenicScore {
+    const treeHeight = grid[i][j]
+
+    let testI, testJ;
+    testI = i-1;
+    while(testI >= 0 && grid[testI][j] < treeHeight) { testI--; }
+    const upScore = i-Math.max(testI,0);
+
+    testJ = j-1;
+    while(testJ >= 0 && grid[i][testJ] < treeHeight) { testJ--; }
+    const leftScore = j-Math.max(testJ,0);
+
+    testI = i+1;
+    while(testI < grid.length && grid[testI][j] < treeHeight) { testI++; }
+    const downScore = Math.min(grid.length-1, testI) - i;
+
+    testJ = j+1;
+    while(testJ < grid[0].length && grid[i][testJ] < treeHeight) { testJ++; }
+    const rightScore = Math.min(grid[0].length-1, testJ) - j;
+
+    return {
+        total: leftScore * rightScore * upScore * downScore,
+        treeHeight, leftScore, rightScore, upScore, downScore
+    }
+}
+
+export function D08_findBestTreeScenicScores(grid: number[][]): {coords: [number,number], score: TreeScenicScore } {
+    const treeScenicScores = cartesian2(
+        Ranged.included(1, grid.length-1).values(),
+        Ranged.included(1, grid[0].length-1).values(),
+    ).map(([i,j]) => {
+        return {coords: [i, j] as [number,number], score: D08_scenicScoreOf(grid, [i,j]) }
+    }, 0)
+
+    treeScenicScores.sort((tss1, tss2) => tss2.score.total - tss1.score.total)
+    return treeScenicScores[0];
+}
--- test/2022-08.test.ts
+++ test/2022-08.test.ts
@@ -1,7 +1,11 @@
 
-import {D08_INPUT, D08_Q1_SAMPLE, D08_Q2_SAMPLE} from "./2022-08.inputs";
-import {D08_countVisibleTrees, D08_isTreePositionVisible, D08_parseInput} from "../src/2022-08";
-// import {D08_superFunctionForQ1, D08_superFunctionForQ2} from "../src/2022-08";
+import {D08_INPUT, D08_Q1_SAMPLE} from "./2022-08.inputs";
+import {
+    D08_countVisibleTrees, D08_findBestTreeScenicScores,
+    D08_isTreePositionVisible,
+    D08_parseInput,
+    D08_scenicScoreOf
+} from "../src/2022-08";
 
 test("Q1 tree visibility tests", () => {
     const grid = D08_parseInput(D08_Q1_SAMPLE)
@@ -23,14 +27,51 @@ test("Q1 Sample", () => {
 test("Q1 Input", () => {
     expect(D08_countVisibleTrees(D08_parseInput(D08_INPUT))).toEqual(1698);
 })
-/*
+
+test("tree scenic scores", () => {
+    const grid = D08_parseInput(D08_Q1_SAMPLE)
+    expect(D08_scenicScoreOf(grid, [1, 2])).toEqual({
+        treeHeight: 5,
+        total: 4,
+        upScore: 1,
+        leftScore: 1,
+        rightScore: 2,
+        downScore: 2
+    })
+    expect(D08_scenicScoreOf(grid, [3, 2])).toEqual({
+        treeHeight: 5,
+        total: 8,
+        upScore: 2,
+        leftScore: 2,
+        rightScore: 2,
+        downScore: 1
+    })
+})
 test("Q2 Sample", () => {
-    expect(D08_superFunctionForQ2(D08_Q2_SAMPLE)).toEqual(42);
+    expect(D08_findBestTreeScenicScores(D08_parseInput(D08_Q1_SAMPLE))).toEqual({
+        coords: [3,2],
+        score: {
+            treeHeight: 5,
+            total: 8,
+            upScore: 2,
+            leftScore: 2,
+            rightScore: 2,
+            downScore: 1
+        }
+    });
 })
 
 test("Q2 Input", () => {
-    expect(D08_superFunctionForQ2(D08_INPUT)).toEqual(42);
+    expect(D08_findBestTreeScenicScores(D08_parseInput(D08_INPUT))).toEqual({
+        coords: [14,49],
+        score: {
+            treeHeight: 8,
+            total: 672280,
+            upScore: 14,
+            leftScore: 49,
+            rightScore: 49,
+            downScore: 20,
+        }
+    });
 })
-
-*/
 
